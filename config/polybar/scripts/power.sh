f4c1562405fab04c3b88eb2a80a3e7428ec50eae
Denis <denis+github@nesomail.com>
2020-02-20 23:27:13 +0100

    new theme

---
+++ config/polybar/scripts/power.sh
@@ -0,0 +1,17 @@
+
+chosen=$(echo -e "Cancel\nLogout\nShutdown\nReboot\nSuspend\nHibernate" | rofi -dmenu -i -l 6 -fullscreen 1)
+# Info about some states are available here:
+
+# https://www.freedesktop.org/software/systemd/man/systemd-sleep.conf.html#Description
+
+if [[ $chosen = "Logout" ]]; then
+	i3-msg exit
+elif [[ $chosen = "Shutdown" ]]; then
+	systemctl poweroff
+elif [[ $chosen = "Reboot" ]]; then
+	systemctl reboot
+elif [[ $chosen = "Suspend" ]]; then
+	systemctl suspend
+elif [[ $chosen = "Hibernate" ]]; then
+	systemctl hibernate
+fi
